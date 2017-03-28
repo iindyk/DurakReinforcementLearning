e8d2c5fd3fe737fc4e4cbdcb097f073b2c2d554c
iindyk <ihor.indyk@gmail.com>
2017-03-27 22:57:16 -0400

    New Basic feature added: logs analysis shown that attack is not performed by pair cards,probably there is bug in nextState

--- src/DurakGame/Player.java
+++ src/DurakGame/Player.java
@@ -11,28 +11,8 @@ import static DurakGame.Game.logger;
  * Created by igor on 11.10.16.
  */
 public abstract class Player {
-    protected String name;
     public State state=new State();
-
-
-    public abstract Card attack() throws Card.TrumpIsNotDefinedException;
-
-    public abstract Card defend(Card attackCard) throws Card.TrumpIsNotDefinedException;
-
-    public boolean canAttack() {
-        this.state.enemyAttack=null;
-        if (Game.cardsOnTable.size()==0) return true;
-        else {
-            for (Card cardTable: Game.cardsOnTable
-                    ) {
-                for (Card cardHand: this.state.hand
-                        ) {
-                    if (cardHand.value==cardTable.value) return true;
-                }
-            }
-        }
-        return false;
-    }
+    protected String name;
 
     public static boolean canAttack(State currentState) {
         currentState.enemyAttack=null;
@@ -45,91 +25,6 @@ public abstract class Player {
                     if (cardHand.value==cardTable.value) return true;
                 }
             }
-        }
-        return false;
-    }
-
-    //is not used now
-    private boolean canDefendList(ArrayList<Card> attack) throws Card.TrumpIsNotDefinedException {
-        if (attack.isEmpty()) return true;
-        this.state.enemyKnownCards.removeAll(attack);
-        this.state.hiddenCards.removeAll(attack);
-        //this.state.enemyAttack.clear();
-        //this.state.enemyAttack.addAll(attack);
-        ArrayList<ArrayList<Card>> possibleDefences = new ArrayList<>();
-        ArrayList<Card> availableHand0=new ArrayList<>();
-        ArrayList<Card> availableHand1=new ArrayList<>();
-        ArrayList<Card> availableHand2=new ArrayList<>();
-        ArrayList<Card> tmp=new ArrayList<>();
-        for (int i = 0; i <this.state.hand.size() ; i++) {
-            tmp.clear();
-            availableHand0.clear();
-            availableHand0.addAll(state.hand);
-            if (state.hand.get(i).beats(attack.get(0))) {
-                tmp.add(state.hand.get(i));
-                availableHand0.remove(state.hand.get(i));
-                if (attack.size() > 1) {
-                    availableHand1.clear();
-                    availableHand1.addAll(availableHand0);
-                    for (Card card :
-                            availableHand0) {
-                        if (card.beats(attack.get(1))) {
-                            availableHand1.remove(card);
-                            tmp.add(card);
-                            if (attack.size() > 2) {
-                                availableHand2.clear();
-                                availableHand2.addAll(availableHand1);
-                                for (Card card1 :
-                                        availableHand1) {
-                                    if (card1.beats(attack.get(2))) {
-                                        availableHand2.remove(card1);
-                                        tmp.add(card1);
-                                        if (attack.size() > 3) {
-                                            for (Card card2 :
-                                                    availableHand2) {
-                                                if (card2.beats(attack.get(3))) {
-                                                    tmp.add(card2);
-                                                    possibleDefences.add(new ArrayList(tmp));
-                                                    tmp.clear();
-                                                }
-                                            }
-                                        }
-                                        else {
-                                            possibleDefences.add(new ArrayList(tmp));
-                                            tmp.remove(2);
-                                        }
-                                    }
-                                }
-                            }
-                            else {
-                                possibleDefences.add(new ArrayList(tmp));
-                                tmp.remove(1);
-                            }
-
-                        }
-                    }
-
-                }
-                else {
-                    possibleDefences.add(new ArrayList(tmp));
-                    tmp.remove(0);
-                }
-
-
-            }
-
-        }
-        return !(possibleDefences.size()==0);
-    }
-
-    public boolean canDefend(Card attackCard) throws Card.TrumpIsNotDefinedException {
-        if (attackCard==null) return true;
-        this.state.enemyKnownCards.remove(attackCard);
-        this.state.hiddenCards.remove(attackCard);
-        this.state.enemyAttack=attackCard;
-        for (Card card:
-             this.state.hand) {
-            if (card.beats(attackCard)) return true;
         }
         return false;
     }
@@ -317,10 +212,7 @@ public abstract class Player {
             }
         }
         else {
-            for (Card hc:
-                 chand) {
-                possibleActions.add(hc);
-            }
+            possibleActions.addAll(chand);
         }
 
         return possibleActions;
@@ -330,6 +222,106 @@ public abstract class Player {
         if (attackCard == null) return true;
         for (Card card :
                 hand) {
+            if (card.beats(attackCard)) return true;
+        }
+        return false;
+    }
+
+    public abstract Card attack() throws Card.TrumpIsNotDefinedException;
+
+    public abstract Card defend(Card attackCard) throws Card.TrumpIsNotDefinedException;
+
+    public boolean canAttack() {
+        this.state.enemyAttack = null;
+        if (Game.cardsOnTable.size() == 0) return true;
+        else {
+            for (Card cardTable : Game.cardsOnTable
+                    ) {
+                for (Card cardHand : this.state.hand
+                        ) {
+                    if (cardHand.value == cardTable.value) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean canDefendList(ArrayList<Card> attack) throws Card.TrumpIsNotDefinedException {
+        if (attack.isEmpty()) return true;
+        this.state.enemyKnownCards.removeAll(attack);
+        this.state.hiddenCards.removeAll(attack);
+        //this.state.enemyAttack.clear();
+        //this.state.enemyAttack.addAll(attack);
+        ArrayList<ArrayList<Card>> possibleDefences = new ArrayList<>();
+        ArrayList<Card> availableHand0 = new ArrayList<>();
+        ArrayList<Card> availableHand1 = new ArrayList<>();
+        ArrayList<Card> availableHand2 = new ArrayList<>();
+        ArrayList<Card> tmp = new ArrayList<>();
+        for (int i = 0; i < this.state.hand.size(); i++) {
+            tmp.clear();
+            availableHand0.clear();
+            availableHand0.addAll(state.hand);
+            if (state.hand.get(i).beats(attack.get(0))) {
+                tmp.add(state.hand.get(i));
+                availableHand0.remove(state.hand.get(i));
+                if (attack.size() > 1) {
+                    availableHand1.clear();
+                    availableHand1.addAll(availableHand0);
+                    for (Card card :
+                            availableHand0) {
+                        if (card.beats(attack.get(1))) {
+                            availableHand1.remove(card);
+                            tmp.add(card);
+                            if (attack.size() > 2) {
+                                availableHand2.clear();
+                                availableHand2.addAll(availableHand1);
+                                for (Card card1 :
+                                        availableHand1) {
+                                    if (card1.beats(attack.get(2))) {
+                                        availableHand2.remove(card1);
+                                        tmp.add(card1);
+                                        if (attack.size() > 3) {
+                                            for (Card card2 :
+                                                    availableHand2) {
+                                                if (card2.beats(attack.get(3))) {
+                                                    tmp.add(card2);
+                                                    possibleDefences.add(new ArrayList(tmp));
+                                                    tmp.clear();
+                                                }
+                                            }
+                                        } else {
+                                            possibleDefences.add(new ArrayList(tmp));
+                                            tmp.remove(2);
+                                        }
+                                    }
+                                }
+                            } else {
+                                possibleDefences.add(new ArrayList(tmp));
+                                tmp.remove(1);
+                            }
+
+                        }
+                    }
+
+                } else {
+                    possibleDefences.add(new ArrayList(tmp));
+                    tmp.remove(0);
+                }
+
+
+            }
+
+        }
+        return !(possibleDefences.size() == 0);
+    }
+
+    public boolean canDefend(Card attackCard) throws Card.TrumpIsNotDefinedException {
+        if (attackCard == null) return true;
+        this.state.enemyKnownCards.remove(attackCard);
+        this.state.hiddenCards.remove(attackCard);
+        this.state.enemyAttack = attackCard;
+        for (Card card :
+                this.state.hand) {
             if (card.beats(attackCard)) return true;
         }
         return false;
